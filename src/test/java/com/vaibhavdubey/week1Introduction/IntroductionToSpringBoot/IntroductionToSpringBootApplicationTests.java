fe03ed6e23b24dc4d861f957e06c84e5e931a0ca
Vaibhav Dubey <vaibhavdubey1419@gmail.com>
2024-08-16 22:46:00 +0530

    Starting of Spring Boot

---
+++ src/test/java/com/vaibhavdubey/week1Introduction/IntroductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavdubey.week1Introduction.IntroductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
